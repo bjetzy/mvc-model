241f9974041b5a96ca1fb4bf7cfddc5a70a35038
bjetzy <yannic.jetzer@bbcag.ch>
2021-10-19 13:38:40 +0200

    added Configuration

---
+++ templates/default/index.php
@@ -0,0 +1,7 @@
+<div id="startPageContent">
+  <div id="imageStartPage">
+    <img src="images/BlogIt_Logo.svg" alt="Logo"/>
+  </div>
+    <p id="startSentence">Loggen Sie sich jetzt ein</p>
+    <button id="toLoginButton"><a href="user/create">Zum Login</a></button>
+</div>
---
+++ templates/user/login.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<form action="/user/doCreate" method="post" class="col-6">
+		<div class="form-group">
+		  <label for="uname">Benutzername</label>
+	  	<input id="uname" name="uname" type="text" class="form-control">
+		</div>
+		<div class="form-group">
+			<label class="control-label" for="password">Passwort</label>
+			<input id="password" name="password" type="password" class="form-control">
+		</div>
+		<button type="submit" name="send" class="btn btn-primary">Absenden</button>
+	</form>
+</div>
